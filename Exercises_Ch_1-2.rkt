ebeaf71e4e20d63e7e21fe3df3b094c01bf8c980
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-05-12 00:09:51 -0400

    Update Exercises_Ch_1-2.rkt

--- Exercises_Ch_1-2.rkt
+++ Exercises_Ch_1-2.rkt
@@ -1,3 +1,27 @@
+; Factorial (page 41-43)
+
+; Recursive solution
+
+(define (fact n)
+  (if (= n 0)
+      1
+      (* n (fact (- n 1)))))
+
+; Iterative
+
+(define (fact n)
+  (define (fact-iter acc n)
+    (if (= n 0)
+        acc
+        (fact-iter (* n acc) (- n 1))))
+  (fact-iter 1 n))
+
+; Algorithms + Threading
+
+(define (fact n)
+  (~> (range 1 (+ n 1))
+      (foldl * 1 _)))
+
 ; Exercise 1.11 (page 53)
 
 ; Recursive solution
