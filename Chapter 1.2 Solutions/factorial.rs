f5646f244a6b381eec9b54256507cf567f518ec0
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-05-17 18:06:17 -0400

    Update factorial.rs

--- Chapter 1.2 Solutions/factorial.rs
+++ Chapter 1.2 Solutions/factorial.rs
@@ -1,7 +1,7 @@
 // https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=dde1720a26e2530d29944c3427480518
 
 fn fact(n: i32) -> i32 {
-    return (1..n+1).fold(1, |acc, n| acc * n);
+    (1..n+1).fold(1, |acc, n| acc * n)
 }
 
 fn main() {
