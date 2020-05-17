4fd21daf97bade82351e04526b86d64d5c5d0f21
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-05-17 12:03:33 -0400

    Create fibonacci.rs

---
+++ Chapter 1.2 Solutions/fibonacci.rs
@@ -0,0 +1,9 @@
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=334c850c417dd1b33059a7ee2855a339
+
+fn fib(n: i32) -> i32 {
+    return (1..n-1).fold((1, 1), |(a, b), _unused| (b, a + b)).1;
+}
+
+fn main() {
+    println!("{:?}", fib(10));
+}
