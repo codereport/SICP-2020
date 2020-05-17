498c6be5b02021235eca8d5b6d62c2bc4027df36
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-05-16 21:59:41 -0400

    Update Exercises_Ch_1-2.rkt

--- Exercises_Ch_1-2.rkt
+++ Exercises_Ch_1-2.rkt
@@ -200,3 +200,38 @@
 ; > (timed-prime-test 10000000000037)
 ; 10000000000037 *** 116
 
+; Exercise 1.27 (page 73)
+
+; Copied from text
+
+; prime? from above
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (square x) (* x x))
+
+; Modified
+
+; Don't want a random number (pass it in)
+(define (fermat-test n a)
+  (= (expmod a n n) a))
+
+; My code
+
+(define (is-carmichael-number? n)
+  (define (fermat-test-all n)
+    (~> (range 1 n)
+        (andmap (λ (a) (fermat-test n a)) _)))
+  (and (fermat-test-all n) (not (prime? n))))
+
+; > (map is-carmichael-number? '(561 1105 1729 2465 2821 6601))
+; '(#t #t #t #t #t #t)
