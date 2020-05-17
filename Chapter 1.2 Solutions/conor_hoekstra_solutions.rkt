7d1fa3972330b9c80952ceab44bd4a1fe66e98e1
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-05-17 11:52:05 -0400

    Update conor_hoekstra_solutions.rkt

--- Chapter 1.2 Solutions/conor_hoekstra_solutions.rkt
+++ Chapter 1.2 Solutions/conor_hoekstra_solutions.rkt
@@ -24,6 +24,26 @@
   (~> (range 1 (+ n 1))
       (foldl * 1 _)))
 
+; Fibonacci (page 47-50)
+
+; Recursive
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+; Iterative
+
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
 ; Exercise 1.11 (page 53)
 
 ; Recursive solution
