cb2c0490ddb9b65dd5dc8612fc7e11640abad474
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-05-19 10:01:59 -0400

    Update fibonacci.rs

--- Chapter 1.2 Solutions/fibonacci.rs
+++ Chapter 1.2 Solutions/fibonacci.rs
@@ -1,7 +1,7 @@
 // https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=334c850c417dd1b33059a7ee2855a339
 
 fn fib(n: i32) -> i32 {
-    (1..n-1).fold((1, 1), |(a, b), _unused| (b, a + b)).1
+    (1..=n).fold((1, 1), |(a, b), _| (b, a + b)).1
 }
 
 fn main() {
