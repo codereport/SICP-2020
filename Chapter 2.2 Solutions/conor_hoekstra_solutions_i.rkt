ad885a2df233bbca6e56bd0a69d371423b6d747e
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-06-14 11:06:03 -0400

    Update conor_hoekstra_solutions_i.rkt

--- Chapter 2.2 Solutions/conor_hoekstra_solutions_i.rkt
+++ Chapter 2.2 Solutions/conor_hoekstra_solutions_i.rkt
@@ -171,3 +171,29 @@
 
 (define (square-tree tree) (tree-map tree sq))
 (define (scale-tree tree factor) (tree-map tree (λ (x) (* x factor))))
+
+;; Exercise 2.32
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (λ (x) (cons (car s) x))  rest)))))
+
+;; > (subsets (range 4))
+;; '(()
+;;   (3)
+;;   (2)
+;;   (2 3)
+;;   (1)
+;;   (1 3)
+;;   (1 2)
+;;   (1 2 3)
+;;   (0)
+;;   (0 3)
+;;   (0 2)
+;;   (0 2 3)
+;;   (0 1)
+;;   (0 1 3)
+;;   (0 1 2)
+;;   (0 1 2 3))
