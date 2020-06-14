7bf687f54a7f60b7ee4d3ad76a6e45661d13dd2c
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-06-13 22:23:14 -0400

    Update and rename conor_hoekstra_solutions.rkt to conor_hoekstra_solutions_i.rkt

--- Chapter 2.2 Solutions/conor_hoekstra_solutions_i.rkt
+++ Chapter 2.2 Solutions/conor_hoekstra_solutions_i.rkt
@@ -75,3 +75,22 @@
   (cond ((null? (cdr lst)) (proc (car lst)))
         (else (proc (car lst))
               (for-each proc (cdr lst)))))
+
+;; Exercise 2.25
+
+> (define x '(1 3 (5 7) 9))
+> (car (cdaddr x))
+;; 7
+
+> (define x '((7)))
+> (caar x)
+;; 7
+
+(require threading)
+
+> (define x '(1 (2 (3 (4 (5 (6 7)))))))
+> (~> x
+      cadadr
+      cadadr
+      cadadr)
+;; 7
