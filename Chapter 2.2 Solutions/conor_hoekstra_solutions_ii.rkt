c4dd6b2423f658ad14dcec5fafdb0396e5382a02
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-06-27 13:47:02 -0400

    Create conor_hoekstra_solutions_ii.rkt

---
+++ Chapter 2.2 Solutions/conor_hoekstra_solutions_ii.rkt
@@ -0,0 +1,43 @@
+;; Hack
+
+(define enumerate-tree flatten)
+(define accumulate foldr)
+(define square sq)
+
+;; Example from the book
+
+(define (sum-odd-squares tree)
+  (accumulate
+   + 0 (map square (filter odd? (enumerate-tree tree)))))
+
+;; With Threading Macro
+
+(require threading)
+
+(define (sum-odd-squares tree)
+  (~>> tree
+       (enumerate-tree)
+       (filter odd?)
+       (map square)
+       (accumulate + 0)))
+       
+;; Exercise 2.33
+
+(define (map p sequence)
+  (accumulate (λ (x y) (cons (p x) y)) `() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq1 seq2))
+
+(define (length sequence)
+  (accumulate (λ (_ acc) (+ acc 1)) 0 sequence))
+
+;; Exercise 2.34 (page 162-3)
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (λ (coeff acc) (+ coeff (* x acc)))
+              0
+              coefficient-sequence))
+
+;; > (horner-eval 2 (list 1 3 0 5 0 1))
+;; 79
