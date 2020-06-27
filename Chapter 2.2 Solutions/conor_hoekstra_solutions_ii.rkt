3af8aeb0d2754c1310bf3d54193b39edb9c3d05b
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-06-27 18:24:20 -0400

    Update conor_hoekstra_solutions_ii.rkt

--- Chapter 2.2 Solutions/conor_hoekstra_solutions_ii.rkt
+++ Chapter 2.2 Solutions/conor_hoekstra_solutions_ii.rkt
@@ -42,7 +42,7 @@
 ;; > (horner-eval 2 (list 1 3 0 5 0 1))
 ;; 79
 
-;; Exericse 2.36
+;; Exericse 2.36 (page 163)
 
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
@@ -52,3 +52,49 @@
 
 ;; > (accumulate-n + 0 '((0 1) (1 2) (2 3)))
 ;; '(3 6)
+
+;; TODO get map + apply solution working
+
+;; Exercise 2.37 (page 163-5)
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+;; > (dot-product (range 4) (range 4))
+;; 14
+
+(define (matrix-*-vector m v)
+  (map (λ (row) (dot-product v row)) m))
+
+;; > (define mat '((0 1 2) (1 2 3) (2 3 4)))
+;; > (matrix-*-vector mat (range 3))
+;; '(5 8 11)
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+;; > (define mat2 '((0 0 0) (1 1 1) (2 2 2)))
+;; > (transpose mat2)
+;; '((0 1 2) (0 1 2) (0 1 2))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (λ (row) (matrix-*-vector cols row))  m)))
+
+;; > (matrix-*-matrix mat mat2)
+;; '((5 5 5) (8 8 8) (11 11 11))
+;; > (matrix-*-matrix mat2 mat)
+;; '((0 0 0) (3 6 9) (6 12 18))
+
+;; Exercise 2.38 (page 165)
+
+;; > (foldr / 1 '(1 2 3))
+;; 1 1/2
+;; > (foldl / 1 '(1 2 3))
+;; 1 1/2
+;; > (foldr list '() '(1 2 3))
+;; '(1 (2 (3 ())))
+;; > (foldl list '() '(1 2 3))
+;; '(3 (2 (1 ())))
+
+;; asssociativity / commutative
