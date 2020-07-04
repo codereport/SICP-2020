65f894707f872068ad5252e92273caeeeb3f2dff
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-07-04 08:26:47 -0400

    Update conor_hoekstra_solutions.rkt

--- Chapter 2.3 Solutions/conor_hoekstra_solutions.rkt
+++ Chapter 2.3 Solutions/conor_hoekstra_solutions.rkt
@@ -122,3 +122,66 @@
 (check-equal? (union-set '(1 2 3) '(3 4)) '(1 2 3 4))
 (check-equal? (union-set '(1 3) '(2 3 4)) '(1 2 3 4))
 
+;; Exercise 2.63 (page 213/14)
+
+;; a) the same (in-order traversal)
+;; b) append vs cons = O(nlogn) vs O(n)
+
+;; Exercise 2.65 (page 216)
+
+;; From book
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (tree->list tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list
+                             (right-branch tree)
+                             result-list)))))
+  (copy-to-list tree '()))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result
+               (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result
+                   (partial-tree
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts
+                     (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+
+;; union-set via balanced-tree
+
+(define (union-set a b)
+  (list->tree
+   (remove-duplicates
+    (append (tree->list a)
+            (tree->list b)))))
+
+;; > (union-set (list->tree '(1 2 3 5 7 9 46))  
+;;              (list->tree '(5 6 10 11 20 23 46))) 
+;; '(9
+;;   (3 (1 () (2 () ())) (5 () (7 () ())))
+;;   (10 (46 () (6 () ())) (20 (11 () ()) (23 () ()))))
