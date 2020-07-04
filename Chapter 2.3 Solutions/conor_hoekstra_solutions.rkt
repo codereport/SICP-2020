5e91904224daa7b19a1fbf106becaab0b7b8b0a7
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-07-04 09:07:07 -0400

    Update conor_hoekstra_solutions.rkt

--- Chapter 2.3 Solutions/conor_hoekstra_solutions.rkt
+++ Chapter 2.3 Solutions/conor_hoekstra_solutions.rkt
@@ -185,3 +185,101 @@
 ;; '(9
 ;;   (3 (1 () (2 () ())) (5 () (7 () ())))
 ;;   (10 (46 () (6 () ())) (20 (11 () ()) (23 () ()))))
+
+;; Code from book
+
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+(define (leaf? object) (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch
+         tree) (car
+                tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+;; Exercise 2.67 (page 226)
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree
+                    (make-leaf 'D 1)
+                    (make-leaf 'C 1)))))
+
+;;    root
+;; 0  /  \  1
+;;   A    .
+;;       / \
+;;   0  B   .  1
+;;         / \
+;;     0  D   c  1
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+;;                       A D     A B   B   C     A
+
+;; Exercise 2.68 (page 226/7)
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol s tree)
+  (if (leaf? tree)
+      (if (eq? s (symbol-leaf tree))
+          '()
+          (error "fail"))
+      (if (memq s (symbols (left-branch tree)))
+          (cons 0 (encode-symbol s (left-branch tree)))
+          (cons 1 (encode-symbol s (right-branch tree))))))
+
+;; > (encode '(A D A B B C A) sample-tree) 
+;; '(0 1 1 0 0 1 0 1 0 1 1 1 0)
