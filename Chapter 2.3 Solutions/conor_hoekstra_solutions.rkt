18120da5290b13994bedb973cae5754eb858fa36
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-07-04 00:41:52 -0400

    Update conor_hoekstra_solutions.rkt

--- Chapter 2.3 Solutions/conor_hoekstra_solutions.rkt
+++ Chapter 2.3 Solutions/conor_hoekstra_solutions.rkt
@@ -10,3 +10,79 @@
      
 (check-equal? (equal? '(0 1 2) (range 3)) #t)
 (check-equal? (equal? '(0 1 2) (range 2)) #f)
+
+;; Symbolic Differentiation (from the book)
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type: DERIV" exp))))
+
+;; Exercise 2.56 (page 203)
+
+(define (make-exponentiation base exp) 
+  (cond ((=number? base 1) 1) 
+        ((=number? exp 1) base) 
+        ((=number? exp 0) 1) 
+        (else (list '^ base exp))))
+
+(define base cadr)
+(define exponent caddr)
+
+(define (exponentiation? exp) 
+  (and (list? exp) (eq? (car exp) '^))) 
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponentiation? exp) 
+         (make-product  
+          (make-product
+           (exponent exp)
+           (make-exponentiation (base exp)
+                                (make-sum (exponent exp) -1)))
+          (deriv (base exp) var)))
+        (else
+         (error "unknown expression type: DERIV" exp))))
+
+(check-equal? (deriv '(^ x 3) 'x) '(* 3 (^ x 2)))
