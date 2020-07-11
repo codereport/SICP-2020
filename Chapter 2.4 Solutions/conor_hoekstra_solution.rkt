3bf54031df2b096814ae8dacfe57f39c71639390
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-07-11 15:51:52 -0400

    Update conor_hoekstra_solution.rkt

--- Chapter 2.4 Solutions/conor_hoekstra_solution.rkt
+++ Chapter 2.4 Solutions/conor_hoekstra_solution.rkt
@@ -34,3 +34,63 @@
 ;;    Why can't we assimilate the predicates number? and variable into the
 ;;      data-directed dispatch? No "tag" to dispatch on
 
+;; b)
+
+;; sum
+
+(define (install-sum-package) 
+  (define make-sum cons) 
+  (define addend cadr) 
+  (define augend caddr) 
+  (define (deriv-sum expr var)
+    (make-sum (deriv (addend expr) var)
+              (deriv (augend expr) var)))   
+  (define (tag x) (attach-tag '+ x))
+  
+  (put 'deriv '(+) deriv-sum) 
+  (put 'make-sum '+ (λ (x y) (tag (make-sum x y))))
+  
+  'done) 
+  
+(define (make-sum x y) ((get 'make-sum '+) x y))
+
+;; product
+
+(define (install-product-package) 
+  (define make-product cons) 
+  (define multiplier cadr) 
+  (define multiplicand caddr) 
+  (define (deriv-product expr var) 
+    (make-sum 
+     (make-product (multiplier expr) 
+                   (deriv (multiplicand expr) var)) 
+     (make-product (deriv (multiplier expr) var) 
+                   (multiplicand expr)))) 
+  
+  (define (tag x) (attach-tag '* x))
+  
+  (put 'deriv '(*) deriv-product) 
+  (put 'make-product '* (λ (x y) (tag (make-product x y))))
+  
+  'done) 
+  
+(define (make-product x y) ((get 'make-product '*) x y)) 
+
+;; boilerplate for get & put
+
+ (define table (make-hash))
+ (define (put key1 key2 value) (hash-set! table (list key1 key2) value))
+ (define (get key1 key2) (hash-ref table (list key1 key2) #f))
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+;; (require racketunit)
+
+(install-sum-package)
+(install-product-package)
