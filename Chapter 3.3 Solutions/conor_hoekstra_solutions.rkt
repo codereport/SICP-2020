955bc673e3d8c9439f6784b9e5d73af731ff3e4e
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-08-04 23:08:58 -0400

    Create conor_hoekstra_solutions.rkt

---
+++ Chapter 3.3 Solutions/conor_hoekstra_solutions.rkt
@@ -0,0 +1,11 @@
+;; Exercise 3.12 (page 345-6)
+
+;; Response from (cdr x) after append
+'(b)
+
+;; Response from (cdr x) after append!
+'(b c d) ; equivalent of passing something by reference and then modifying
+
+;; Exercise 3.14 (page 347)
+
+;; mystery reverses a list
