7a895d5d51a4833936bfddbd7fb3d30e0172da8f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-08-21 00:10:26 -0400

    Update conor_hoekstra_solutions.scm

--- Chapter 3.5 Solutions/conor_hoekstra_solutions.scm
+++ Chapter 3.5 Solutions/conor_hoekstra_solutions.scm
@@ -97,3 +97,77 @@
 
 ;; Test
 (stream-ref (partial-sums (stream-enumerate-interval 1 5)) 4) ; 15
+
+;; Exercise 3.56 (page 448-49)
+
+;; code from the book
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor))
+              stream))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream
+                   s1car
+                   (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (cons-stream
+                   s2car
+                   (merge s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream
+                   s1car
+                   (merge (stream-cdr s1)
+                          (stream-cdr s2)))))))))
+
+(define S (cons-stream 1 (merge (scale-stream S 2)
+                                (merge (scale-stream S 3)
+                                       (scale-stream S 5)))))
+
+;; Test
+(map (lambda (x) (stream-ref S x))
+     '(0 1 2 3 4 5 6 7 8 9 10)) ; (1 2 3 4 5 6 8 9 10 12 15)
+
+;; Pi from alternating series
+
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+;; Pi with Euler acceleration
+
+(define (square x) (* x x))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))  ; S n -1
+        (s1 (stream-ref s 1))  ; S n
+        (s2 (stream-ref s 2))) ; S n+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+;; Pi with super-"tableu" acceleration
+
+(define (make-tableau transform s)
+  (cons-stream s (make-tableau transform (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car (make-tableau transform s)))
+
+(stream-ref pi-stream 1000)       ; 3.1425916543395442
+
+(stream-ref (euler-transform
+             pi-stream) 1000)     ; 3.1415926538383
+
+(stream-ref (accelerated-sequence
+             euler-transform
+             pi-stream) 9)        ;3.141592653589795
