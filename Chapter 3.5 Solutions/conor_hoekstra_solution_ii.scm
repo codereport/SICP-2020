4a699493020c9349578e3f9dbf5024dc35b141ca
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-08-27 22:31:28 -0400

    Update conor_hoekstra_solution_ii.scm

--- Chapter 3.5 Solutions/conor_hoekstra_solution_ii.scm
+++ Chapter 3.5 Solutions/conor_hoekstra_solution_ii.scm
@@ -30,3 +30,51 @@
 
 (stream-ref zero-crossings2 0) ;  1
 (stream-ref zero-crossings2 1) ; -1
+
+;; Exercise 3.77 (page 473)
+
+;; code from the book
+
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream
+     initial-value
+     (let ((integrand (force delayed-integrand)))
+       (add-streams (scale-stream integrand dt) int))))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(stream-ref (solve (lambda (x) x) 1 0.001) 1000)
+; y: undefined;
+; cannot use before initialization
+
+;; original
+(define (integral integrand initial-value dt)
+  (cons-stream
+   initial-value
+   (if (stream-null? integrand)
+       the-empty-stream
+       (integral (stream-cdr integrand)
+                 (+ (* dt (stream-car integrand))
+                    initial-value)
+                 dt))))
+
+;; modified
+(define (integral delayed-integrand initial-value dt) 
+  (cons-stream
+   initial-value 
+   (let ((integrand (force delayed-integrand))) 
+     (if (stream-null? integrand) 
+         the-empty-stream 
+         (integral (delay (stream-cdr integrand)) 
+                   (+ (* dt (stream-car integrand)) 
+                      initial-value) 
+                   dt)))))
+
+;; Exercise 3.81 (page 481)
+
+;; TODO
