7edda565b61e6b00825e8cabd05b88c3b92cdfd3
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-05 15:33:56 -0400

    Update conor_hoekstra_solutions.rkt

--- Chapter 4.2 Solutions/conor_hoekstra_solutions.rkt
+++ Chapter 4.2 Solutions/conor_hoekstra_solutions.rkt
@@ -1,6 +1,7 @@
 ;; Exercise 4.25 (543-4)
 
-;; Applicative order will run forever - because base case will never be hit "alone" - both arguments will always be evaluated.
+;; Applicative order will run forever - because base case will never be hit "alone" 
+;;     - both arguments will always be evaluated.
 ;; Normal order will not have this problem and will work fine.
 
 ;; CODE FROM BOOK - changes to make to 4.1 Scheme Interpreter
