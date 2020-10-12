093284926f5a479f57f19f16fc2c56d8f92535d4
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-10-12 18:27:53 -0400

    Update conor_hoekstra_solution.scm

--- Chapter 4.3 Solutions/conor_hoekstra_solution.scm
+++ Chapter 4.3 Solutions/conor_hoekstra_solution.scm
@@ -257,3 +257,35 @@
         (else
          (error "Unknown procedure type: EXECUTE-APPLICATION"
                 proc))))
+
+(define input-prompt  ";;; Amb-Eval input:")
+(define output-prompt ";;; Amb-Eval value:")
+
+(define (driver-loop)
+  (define (internal-loop try-again)
+    (prompt-for-input input-prompt)
+    (let ((input (read)))
+      (if (eq? input 'try-again)
+          (try-again)
+          (begin
+            (newline) (display ";;; Starting a new problem ")
+            (ambeval
+             input
+             the-global-environment
+             ;; ambeval success
+             (lambda (val next-alternative)
+               (announce-output output-prompt)
+               (user-print val)
+               (internal-loop next-alternative))
+             ;; ambeval failure
+             (lambda ()
+               (announce-output
+                ";;; There are no more values of")
+               (user-print input)
+               (driver-loop)))))))
+  (internal-loop
+   (lambda ()
+     (newline) (display ";;; There is no current problem")
+     (driver-loop))))
+
+(define (require p) (if (not p) (amb)))
