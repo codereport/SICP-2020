25a0c16e6590e2407b3d0bfe5f6553445d2909f3
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-12-13 12:59:34 -0500

    Update conor_hoekstra_solutions.rkt

--- Chapter 5.2 Solutions/conor_hoekstra_solutions.rkt
+++ Chapter 5.2 Solutions/conor_hoekstra_solutions.rkt
@@ -383,11 +383,11 @@
      here
      (assign a (const 3))
      (goto (label there))
-    here
-    (assign a (const 4))
-    (goto (label there))
-    there
-    (perform (op print) (reg a)))))
+     here
+     (assign a (const 4))
+     (goto (label there))
+     there
+     (perform (op print) (reg a)))))
 
 (start exercise-5-8) ; 3
 
